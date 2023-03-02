2fac84a24c567088d2b348c0914fdc961a9e7d88
Jorge Rausell <jrausell@gmail.com>
2023-03-02 21:46:14 +0100

    still not working

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e3e3ee2a23f9d4256153');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8aad882b418d324e52c1');
--- my-block.php
+++ my-block.php
@@ -8,12 +8,15 @@
  */
 
 function create_block_my_block_block_init() {
-    register_block_type( __DIR__ );
-
     // Load MO files for PHP.
     load_plugin_textdomain( 'my-block', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 
+    register_block_type( __DIR__ );
+}
+add_action( 'init', 'create_block_my_block_block_init' );
+
+function script_translations(){
     // Load JSON files for JS - this is necessary if using a custom languages path!!
     wp_set_script_translations( 'my-block-local-edit-script', 'my-block', plugin_dir_path( __FILE__ ) . '/languages' );
 }
-add_action( 'init', 'create_block_my_block_block_init' );
+add_action( 'wp_enqueue_script', 'script_translations' );
